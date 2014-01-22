9e748b24b0cd178a87aae765d70d183252068a23
Armen Markossyan <a.a.markossyan@gmail.com>
2014-01-22 15:34:42 +0600

    Makes namespace autoloading portable

--- engine/src/Mobileka/L3/Engine/start.php
+++ engine/src/Mobileka/L3/Engine/start.php
@@ -1,27 +1,27 @@
 <?php
 
 Autoloader::namespaces(array(
-  'Mobileka\L3\Engine' => '/home/galymzhan/work/lara3/lara3.dev/laravel-engine/engine/src/Mobileka/L3/Engine/',
+  'Mobileka\L3\Engine' => __DIR__
 ));
 
-Laravel\Autoloader::$aliases['Bundle'] = 'Laravel\\Bundle';
-Laravel\Autoloader::$aliases['Config'] = 'Laravel\\Config';
+Laravel\Autoloader::$aliases['Bundle'] = 'Laravel\Bundle';
+Laravel\Autoloader::$aliases['Config'] = 'Laravel\Config';
 Laravel\Autoloader::$aliases['Controller'] = 'Mobileka\L3\Engine\Laravel\Base\Controller';
 Laravel\Autoloader::$aliases['BaseModel'] = 'Mobileka\L3\Engine\Laravel\Base\Model';
-Laravel\Autoloader::$aliases['View'] = 'Mobileka\L3\Engine\Laravel\Base\\View';
-Laravel\Autoloader::$aliases[ 'File'] = 'Laravel\\File';
-Laravel\Autoloader::$aliases['HTML'] = 'Laravel\\HTML';
-Laravel\Autoloader::$aliases['Input'] = 'Laravel\\Input';
-Laravel\Autoloader::$aliases['Lang'] = 'Laravel\\Lang';
-Laravel\Autoloader::$aliases['URL'] = 'Laravel\\URL';
-Laravel\Autoloader::$aliases['Redirect'] = 'Laravel\\Redirect';
-Laravel\Autoloader::$aliases['Router'] = 'Laravel\\Routing\\Router';
-Laravel\Autoloader::$aliases['Session'] = 'Laravel\\Session';
-Laravel\Autoloader::$aliases['Str'] = 'Laravel\\Str';
-Laravel\Autoloader::$aliases['Validator'] = 'Laravel\\Validator';
-Laravel\Autoloader::$aliases['BaseClass'] = 'Base\\BClass';
-Laravel\Autoloader::$aliases['Debug'] = 'Mobileka\L3\Engine\Laravel\Helpers\\Debug';
-Laravel\Autoloader::$aliases['Misc'] = 'Mobileka\L3\Engine\Laravel\Helpers\\Misc';
-Laravel\Autoloader::$aliases['Arr'] = 'Mobileka\L3\Engine\Laravel\Helpers\\Arr';
-Laravel\Autoloader::$aliases['Carbon'] = 'Carbon\\Carbon';
-Laravel\Autoloader::$aliases['Sphinx'] = 'Mobileka\\Sphinx\\Sphinx';
+Laravel\Autoloader::$aliases['View'] = 'Mobileka\L3\Engine\Laravel\Base\View';
+Laravel\Autoloader::$aliases['File'] = 'Laravel\File';
+Laravel\Autoloader::$aliases['HTML'] = 'Laravel\HTML';
+Laravel\Autoloader::$aliases['Input'] = 'Laravel\Input';
+Laravel\Autoloader::$aliases['Lang'] = 'Laravel\Lang';
+Laravel\Autoloader::$aliases['URL'] = 'Laravel\URL';
+Laravel\Autoloader::$aliases['Redirect'] = 'Laravel\Redirect';
+Laravel\Autoloader::$aliases['Router'] = 'Laravel\Routing\Router';
+Laravel\Autoloader::$aliases['Session'] = 'Laravel\Session';
+Laravel\Autoloader::$aliases['Str'] = 'Laravel\Str';
+Laravel\Autoloader::$aliases['Validator'] = 'Laravel\Validator';
+Laravel\Autoloader::$aliases['BaseClass'] = 'Base\BClass';
+Laravel\Autoloader::$aliases['Debug'] = 'Mobileka\L3\Engine\Laravel\Helpers\Debug';
+Laravel\Autoloader::$aliases['Misc'] = 'Mobileka\L3\Engine\Laravel\Helpers\Misc';
+Laravel\Autoloader::$aliases['Arr'] = 'Mobileka\L3\Engine\Laravel\Helpers\Arr';
+Laravel\Autoloader::$aliases['Carbon'] = 'Carbon\Carbon';
+Laravel\Autoloader::$aliases['Sphinx'] = 'Mobileka\Sphinx\Sphinx';

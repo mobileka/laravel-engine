bb53eae45bb4dadec86fb75ebbee1a498dcfb056
Armen Markossyan <a.a.markossyan@gmail.com>
2014-01-22 16:55:10 +0600

    Adds aliases for laravel-engine components

--- engine/src/Mobileka/L3/Engine/start.php
+++ engine/src/Mobileka/L3/Engine/start.php
@@ -4,24 +4,65 @@ Autoloader::namespaces(array(
   'Mobileka\L3\Engine' => __DIR__
 ));
 
-Laravel\Autoloader::$aliases['Bundle'] = 'Laravel\Bundle';
-Laravel\Autoloader::$aliases['Config'] = 'Laravel\Config';
+Laravel\Autoloader::$aliases['Bundle'] = 'Mobileka\L3\Engine\Laravel\Bundle';
+Laravel\Autoloader::$aliases['Config'] = 'Mobileka\L3\Engine\Laravel\Config';
 Laravel\Autoloader::$aliases['Controller'] = 'Mobileka\L3\Engine\Laravel\Base\Controller';
 Laravel\Autoloader::$aliases['BaseModel'] = 'Mobileka\L3\Engine\Laravel\Base\Model';
 Laravel\Autoloader::$aliases['View'] = 'Mobileka\L3\Engine\Laravel\Base\View';
-Laravel\Autoloader::$aliases['File'] = 'Laravel\File';
-Laravel\Autoloader::$aliases['HTML'] = 'Laravel\HTML';
-Laravel\Autoloader::$aliases['Input'] = 'Laravel\Input';
-Laravel\Autoloader::$aliases['Lang'] = 'Laravel\Lang';
-Laravel\Autoloader::$aliases['URL'] = 'Laravel\URL';
-Laravel\Autoloader::$aliases['Redirect'] = 'Laravel\Redirect';
-Laravel\Autoloader::$aliases['Router'] = 'Laravel\Routing\Router';
-Laravel\Autoloader::$aliases['Session'] = 'Laravel\Session';
-Laravel\Autoloader::$aliases['Str'] = 'Laravel\Str';
-Laravel\Autoloader::$aliases['Validator'] = 'Laravel\Validator';
-Laravel\Autoloader::$aliases['BaseClass'] = 'Base\BClass';
+Laravel\Autoloader::$aliases['File'] = 'Mobileka\L3\Engine\Laravel\File';
+Laravel\Autoloader::$aliases['HTML'] = 'Mobileka\L3\Engine\Laravel\HTML';
+Laravel\Autoloader::$aliases['Input'] = 'Mobileka\L3\Engine\Laravel\Input';
+Laravel\Autoloader::$aliases['Lang'] = 'Mobileka\L3\Engine\Laravel\Lang';
+Laravel\Autoloader::$aliases['URL'] = 'Mobileka\L3\Engine\Laravel\URL';
+Laravel\Autoloader::$aliases['Redirect'] = 'Mobileka\L3\Engine\Laravel\Redirect';
+Laravel\Autoloader::$aliases['Router'] = 'Mobileka\L3\Engine\Laravel\Router';
+Laravel\Autoloader::$aliases['Session'] = 'Mobileka\L3\Engine\Laravel\Session';
+Laravel\Autoloader::$aliases['Str'] = 'Mobileka\L3\Engine\Laravel\Str';
+Laravel\Autoloader::$aliases['Validator'] = 'Mobileka\L3\Engine\Laravel\Validator';
+Laravel\Autoloader::$aliases['BaseClass'] = 'Mobileka\L3\Engine\Laravel\Base\BClass';
+Laravel\Autoloader::$aliases['Image'] = 'Mobileka\L3\Engine\Laravel\Image';
 Laravel\Autoloader::$aliases['Debug'] = 'Mobileka\L3\Engine\Laravel\Helpers\Debug';
 Laravel\Autoloader::$aliases['Misc'] = 'Mobileka\L3\Engine\Laravel\Helpers\Misc';
 Laravel\Autoloader::$aliases['Arr'] = 'Mobileka\L3\Engine\Laravel\Helpers\Arr';
 Laravel\Autoloader::$aliases['Carbon'] = 'Carbon\Carbon';
-Laravel\Autoloader::$aliases['Sphinx'] = 'Mobileka\Sphinx\Sphinx';
+
+Laravel\Autoloader::$aliases['RestfulRouter'] = 'Mobileka\L3\Engine\Laravel\RestfulRouter';
+Laravel\Autoloader::$aliases['Grid'] = 'Mobileka\L3\Engine\Grid\Grid';
+Laravel\Autoloader::$aliases['Form'] = 'Mobileka\L3\Engine\Form\Form';
+
+//Form Components
+Laravel\Autoloader::$aliases['AutocompleteField'] = 'Mobileka\L3\Engine\Form\Components\Autocomplete';
+Laravel\Autoloader::$aliases['CKEditorField'] = 'Mobileka\L3\Engine\Form\Components\CKEditor';
+Laravel\Autoloader::$aliases['DatepickerField'] = 'Mobileka\L3\Engine\Form\Components\Datepicker';
+Laravel\Autoloader::$aliases['DropdownField'] = 'Mobileka\L3\Engine\Form\Components\Dropdown';
+Laravel\Autoloader::$aliases['ChosenDropdownField'] = 'Mobileka\L3\Engine\Form\Components\DropdownChosen';
+Laravel\Autoloader::$aliases['DualMultiselectField'] = 'Mobileka\L3\Engine\Form\Components\DualMultiselect';
+Laravel\Autoloader::$aliases['EmailField'] = 'Mobileka\L3\Engine\Form\Components\Email';
+Laravel\Autoloader::$aliases['ModelListField'] = 'Mobileka\L3\Engine\Form\Components\ModelList';
+Laravel\Autoloader::$aliases['MultiuploadField'] = 'Mobileka\L3\Engine\Form\Components\MultiUpload';
+Laravel\Autoloader::$aliases['PasswordField'] = 'Mobileka\L3\Engine\Form\Components\Password';
+Laravel\Autoloader::$aliases['PriceField'] = 'Mobileka\L3\Engine\Form\Components\Price';
+Laravel\Autoloader::$aliases['RadioField'] = 'Mobileka\L3\Engine\Form\Components\Radio';
+Laravel\Autoloader::$aliases['UploadField'] = 'Mobileka\L3\Engine\Form\Components\SingleUpload';
+Laravel\Autoloader::$aliases['SpinnerField'] = 'Mobileka\L3\Engine\Form\Components\Spinner';
+Laravel\Autoloader::$aliases['TagField'] = 'Mobileka\L3\Engine\Form\Components\Tag';
+Laravel\Autoloader::$aliases['TextField'] = 'Mobileka\L3\Engine\Form\Components\Text';
+Laravel\Autoloader::$aliases['TextareaField'] = 'Mobileka\L3\Engine\Form\Components\TextArea';
+
+//Grid Components
+Laravel\Autoloader::$aliases['TextColumn'] = 'Mobileka\L3\Engine\Grid\Components\Column';
+Laravel\Autoloader::$aliases['NestedTextColumn'] = 'Mobileka\L3\Engine\Grid\Components\ColumnNested';
+Laravel\Autoloader::$aliases['DateColumn'] = 'Mobileka\L3\Engine\Grid\Components\Date';
+Laravel\Autoloader::$aliases['ImageColumn'] = 'Mobileka\L3\Engine\Grid\Components\Image';
+Laravel\Autoloader::$aliases['LinkColumn'] = 'Mobileka\L3\Engine\Grid\Components\Link';
+Laravel\Autoloader::$aliases['PriceColumn'] = 'Mobileka\L3\Engine\Grid\Components\Price';
+Laravel\Autoloader::$aliases['SwitcherColumn'] = 'Mobileka\L3\Engine\Grid\Components\Switcher';
+
+//Filters
+Laravel\Autoloader::$aliases['ContainsFilter'] = 'Mobileka\L3\Engine\Grid\Filters\Contains';
+Laravel\Autoloader::$aliases['DateFilter'] = 'Mobileka\L3\Engine\Grid\Filters\Date';
+Laravel\Autoloader::$aliases['DateRangeFilter'] = 'Mobileka\L3\Engine\Grid\Filters\DateRange';
+Laravel\Autoloader::$aliases['DropdownFilter'] = 'Mobileka\L3\Engine\Grid\Filters\Dropdown';
+Laravel\Autoloader::$aliases['EndsWithFilter'] = 'Mobileka\L3\Engine\Grid\Filters\EndsWith';
+Laravel\Autoloader::$aliases['StartsWithFilter'] = 'Mobileka\L3\Engine\Grid\Filters\StartsWith';
+Laravel\Autoloader::$aliases['TextFilter'] = 'Mobileka\L3\Engine\Grid\Filters\Text';

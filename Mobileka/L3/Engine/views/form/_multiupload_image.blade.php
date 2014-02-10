3e29a8615d42a733612158290cd47e86d20bd1df
Armen Markossyan <a.a.markossyan@gmail.com>
2014-02-10 16:48:27 +0600

    Fixes a hardcoded name of a featuredImageSelector in MultiUpload CRUD component

--- Mobileka/L3/Engine/views/form/_multiupload_image.blade.php
+++ Mobileka/L3/Engine/views/form/_multiupload_image.blade.php
@@ -1,4 +1,4 @@
-<div class="thumbnail thumbnail-plupload {{ (isset($component) && $component->featuredImageSelector && $component->row->image_id == $file->id) ? 'featured-image' : '' }}">
+<div class="thumbnail thumbnail-plupload {{ (isset($component) && $component->featuredImageSelector && $component->row->{$component->featuredImageSelector} == $file->id) ? 'featured-image' : '' }}">
 	<a href="{{ URL::to_route('uploads_admin_default_destroy', $file->id) }}" class="plupload-image-delete"><i class="glyphicon-circle_remove"></i></a>
 	@if ($file->isImage())
 		<img src="{{ $file->proudct_block_sidebar }}" class="featured-image-selector" data-id="{{ $file->id }}">
--- Mobileka/L3/Engine/views/form/multiupload.blade.php
+++ Mobileka/L3/Engine/views/form/multiupload.blade.php
@@ -1,24 +1,14 @@
 <div class="plupload"></div>
 {{ Form::hidden('upload_token', Input::old('upload_token', uniqid())) }}
 
-@if ($component->featuredImageSelector)
-	{{ Form::hidden('image_id', $component->row->image_id, array('id' => 'image_id')) }}
+@if ($fis = $component->featuredImageSelector)
+	{{ Form::hidden($fis, $component->row->image_id, array('id' => $fis)) }}
 @endif
 
 <div class="plupload-images {{ $component->featuredImageSelector ? 'plupload-images-featured-selector' : '' }}">
 	@if (count($component->files))
 		@foreach ($component->files as $file)
-			{{--include('engine::form._multiupload_image')--}}
-
-			<div class="thumbnail thumbnail-plupload {{ ($component->featuredImageSelector && $component->row->image_id == $file->id) ? 'featured-image' : '' }}">
-				<a href="{{ URL::to_route($component->requestId.'_destroy_file', $file->id) }}" class="plupload-image-delete"><i class="glyphicon-circle_remove"></i></a>
-				@if ($file->isImage())
-					<img src="{{ $file->proudct_block_sidebar }}" class="featured-image-selector" data-id="{{ $file->id }}">
-				@else
-					<div class="file-attachment-extension">{{ $file->extension }}</div>
-				@endif
-			</div>
-
+			@include('crud::form._multiupload_image')
 		@endforeach
 	@endif
 </div>

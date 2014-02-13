de98d82a7a781368b13a740ef138fab08206bce0
Galymzhan <kozhayev@gmail.com>
2014-02-13 19:01:14 +0600

    extend i18n from Laramodel

--- Mobileka/L3/Engine/Models/i18n.php
+++ Mobileka/L3/Engine/Models/i18n.php
@@ -1,6 +1,6 @@
 <?php namespace Mobileka\L3\Engine\Models;
 
-class i18n extends \Aware {
+class i18n extends \Mobileka\L3\Engine\Base\Laramodel {
 	public static $timestamps = true;
 	public static $table = 'i18n';
 }

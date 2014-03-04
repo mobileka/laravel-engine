39784caf1de4500bbe22231180da371bc26405f9
Armen Markossyan <a.a.markossyan@gmail.com>
2014-03-04 15:31:13 +0600

    Adds defaultValue to Radio component of the Engine

--- Mobileka/L3/Engine/Form/Components/Radio.php
+++ Mobileka/L3/Engine/Form/Components/Radio.php
@@ -5,6 +5,12 @@ class Radio extends Checkbox {
 	protected $template = 'engine::form.radio';
 	protected $htmlElement = 'radio';
 	protected $options = array();
+	protected $defaultValue = '';
 	protected $requiredAttributes = array('class' => 'icheck-me');
+
+	public function value($lang = '')
+	{
+		return parent::value($lang) ? : $this->defaultValue;
+	}
 
 }

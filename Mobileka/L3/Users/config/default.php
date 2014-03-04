8bd8581de670be737a94395f1af3c876f08cfbee
Daniil <daniil.rakhmatulin@gmail.com>
2014-03-04 10:04:38 +0600

    removes round corner select box in user config

--- Mobileka/L3/Users/config/default.php
+++ Mobileka/L3/Users/config/default.php
@@ -3,7 +3,7 @@
 use Mobileka\L3\Engine\Form\Components\Text,
 	Mobileka\L3\Engine\Form\Components\TextArea,
 	Mobileka\L3\Engine\Form\Components\Password,
-	Mobileka\L3\Engine\Form\Components\Dropdown,
+	Mobileka\L3\Engine\Form\Components\DropdownChosen,
 	Mobileka\L3\Engine\Grid\Components\Column,
 	Mobileka\L3\Engine\Grid\Filters\Contains,
 	Mobileka\L3\Engine\Grid\Filters\Dropdown as DropdownFilter;
@@ -16,7 +16,7 @@ return array(
 		'components' => array(
 			'email' => Text::make('email'),
 			'password' => Password::make('password'),
-			'group_id' => Dropdown::make('group_id')->options($groups),
+			'group_id' => DropdownChosen::make('group_id')->options($groups),
 			'name' => Text::make('name'),
 			'contacts' => TextArea::make('contacts'),
 		),

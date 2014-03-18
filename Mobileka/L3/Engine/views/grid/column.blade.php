84c540eeaf2ad837510990a42e5c1992d7c7f4f4
Armen Markossyan <a.a.markossyan@gmail.com>
2014-03-18 16:49:14 +0600

    Moves text escape to Column class. This makes the column view context-independent.

--- Mobileka/L3/Engine/views/grid/column.blade.php
+++ Mobileka/L3/Engine/views/grid/column.blade.php
@@ -1,9 +1,5 @@
 <?php try { ?>
 
-@if ($component->raw)
 {{ $component->value($lang) }}
-@else
-{{ nl2br(HTML::entities($component->value($lang))) }}
-@endif
 
 <?php } catch (Exception $e) { exit($e->getMessage()); } ?>

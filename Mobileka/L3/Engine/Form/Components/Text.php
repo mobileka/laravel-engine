9fd40bdbe337ff2882634d1b66998a35d14918eb
Armen Markossyan <a.a.markossyan@gmail.com>
2014-03-19 14:06:26 +0600

    Adds prefix and suffix attributes to a Text component

--- Mobileka/L3/Engine/Form/Components/Text.php
+++ Mobileka/L3/Engine/Form/Components/Text.php
@@ -1,7 +1,7 @@
 <?php namespace Mobileka\L3\Engine\Form\Components;
 
 class Text extends BaseComponent {
-
 	protected $template = 'engine::form.text';
-
+	protected $prefix = '';
+	protected $suffix = '';
 }
--- Mobileka/L3/Engine/views/form/text.blade.php
+++ Mobileka/L3/Engine/views/form/text.blade.php
@@ -1,5 +1,7 @@
 <?php try { ?>
 
+{{ $component->prefix }}
 {{ Form::text($name, Input::old($inputOldName, $component->value($lang)), $component->attributes) }}
+{{ $component->suffix }}
 
 <?php } catch (Exception $e) { exit($e->getMessage()); } ?>

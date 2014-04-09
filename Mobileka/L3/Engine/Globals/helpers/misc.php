d3f4ca301a05605776293318628acf29e5efb362
Armen Markossyan <a.a.markossyan@gmail.com>
2014-04-10 01:10:09 +0600

    Fixes a bug in helper naming

--- Mobileka/L3/Engine/Globals/helpers/misc.php
+++ Mobileka/L3/Engine/Globals/helpers/misc.php
@@ -10,7 +10,12 @@ function money($number)
 	return number_format($number, 2, '.', ' ');
 }
 
-function csrf_token()
+function token()
 {
 	return Session::token();
+}
+
+function csrf_meta_tag($view = 'engine::_system.csrf')
+{
+	return View::make($view)->render();
 }

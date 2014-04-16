df9240d0d2c8a2077d6645bc555eb31aa64c35ac
Armen Markossyan <a.a.markossyan@gmail.com>
2014-04-16 17:28:59 +0600

    Removes default escaping and nl2br for CKEditor component

--- Mobileka/L3/Engine/Form/Components/CKEditor.php
+++ Mobileka/L3/Engine/Form/Components/CKEditor.php
@@ -1,8 +1,8 @@
 <?php namespace Mobileka\L3\Engine\Form\Components;
 
 class CKEditor extends BaseComponent {
-
 	protected $template = 'engine::form.ckeditor';
 	protected $htmlElement = 'textarea';
-
+	protected $escape = false;
+	protected $nl2br = false;
 }

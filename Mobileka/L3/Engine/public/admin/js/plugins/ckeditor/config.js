8bc7d40169bf0fd90a3bac833bb86b5de310bf2a
Roman <r.volkov@ibecsystems.kz>
2014-04-16 17:24:43 +0600

    Fixes the error with CKEditor adding empty paragraphs all the time.

--- Mobileka/L3/Engine/public/admin/js/plugins/ckeditor/config.js
+++ Mobileka/L3/Engine/public/admin/js/plugins/ckeditor/config.js
@@ -7,10 +7,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.filebrowserBrowseUrl = '/bundles/admin/js/plugins/ckfinder/ckfinder.html';
+	config.filebrowserBrowseUrl      = '/bundles/admin/js/plugins/ckfinder/ckfinder.html';
 	config.filebrowserImageBrowseUrl = '/bundles/admin/js/plugins/ckfinder/ckfinder.html?type=Images';
 	config.filebrowserFlashBrowseUrl = '/bundles/admin/js/plugins/ckfinder/ckfinder.html?type=Flash';
-	config.filebrowserUploadUrl = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+	config.filebrowserUploadUrl      = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
 	config.filebrowserImageUploadUrl = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
 	config.filebrowserFlashUploadUrl = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+
+	/* Added after weird behaviour with empty paragraphs began. */
+	config.enterMode                 = CKEDITOR.ENTER_BR;
+	config.fillEmptyBlocks           = false;
+	config.AutoParagraph             = false;
 };

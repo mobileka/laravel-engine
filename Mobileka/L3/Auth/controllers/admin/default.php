a360ff283c3bd193dea7ab5b3f7fd5bd59de1f63
Armen Markossyan <a.a.markossyan@gmail.com>
2014-04-17 18:43:04 +0600

    Fixes a Auth redirect loop

--- Mobileka/L3/Auth/controllers/admin/default.php
+++ Mobileka/L3/Auth/controllers/admin/default.php
@@ -18,7 +18,7 @@ class Auth_Admin_Default_Controller extends BackendController {
 	{
 		if (uid())
 		{
-			return Redirect::to_route('admin_home');
+			return Redirect::to_route('auth_admin_default_login');
 		}
 
 		$this->layout->renderView();

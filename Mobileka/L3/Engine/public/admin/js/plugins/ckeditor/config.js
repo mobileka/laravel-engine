8322015ea02eace166f52c575b86bf8c3cfc928e
Roman <r.volkov@ibecsystems.kz>
2014-04-22 13:11:33 +0600

    Fixes the config file for the CKEditor.

--- Mobileka/L3/Engine/public/admin/js/plugins/ckeditor/config.js
+++ Mobileka/L3/Engine/public/admin/js/plugins/ckeditor/config.js
@@ -7,15 +7,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.filebrowserBrowseUrl      = '/bundles/admin/js/plugins/ckfinder/ckfinder.html';
-	config.filebrowserImageBrowseUrl = '/bundles/admin/js/plugins/ckfinder/ckfinder.html?type=Images';
-	config.filebrowserFlashBrowseUrl = '/bundles/admin/js/plugins/ckfinder/ckfinder.html?type=Flash';
-	config.filebrowserUploadUrl      = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-	config.filebrowserImageUploadUrl = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-	config.filebrowserFlashUploadUrl = '/bundles/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
-
-	/* Added after weird behaviour with empty paragraphs began. */
-	config.enterMode                 = CKEDITOR.ENTER_BR;
-	config.fillEmptyBlocks           = false;
-	config.AutoParagraph             = false;
+	config.filebrowserBrowseUrl      = '/bundles/engine/admin/js/plugins/ckfinder/ckfinder.html';
+	config.filebrowserImageBrowseUrl = '/bundles/engine/admin/js/plugins/ckfinder/ckfinder.html?type=Images';
+	config.filebrowserFlashBrowseUrl = '/bundles/engine/admin/js/plugins/ckfinder/ckfinder.html?type=Flash';
+	config.filebrowserUploadUrl      = '/bundles/engine/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+	config.filebrowserImageUploadUrl = '/bundles/engine/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+	config.filebrowserFlashUploadUrl = '/bundles/engine/admin/js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 };

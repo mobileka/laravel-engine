93fa6b0c8b8d486e835b109b1369770d30cee257
Armen Markossyan <a.a.markossyan@gmail.com>
2014-04-23 11:12:37 +0600

    Converts Users bundle to RestfulRouter routes

--- Mobileka/L3/Users/routes.php
+++ Mobileka/L3/Users/routes.php
@@ -13,12 +13,12 @@ Route::filter('before', function()
 	}
 });
 
-Route::get('users/emails', array('as' => 'users_default_emails', 'uses' => 'users::default@emails'));
-Route::get('users/byEmail', array('as' => 'users_default_byEmail', 'uses' => 'users::default@byEmail'));
+RestfulRouter::get('users/emails', array('as' => 'users_default_emails', 'uses' => 'users::default@emails'));
+RestfulRouter::get('users/byEmail', array('as' => 'users_default_byEmail', 'uses' => 'users::default@byEmail'));
 
 RestfulRouter::make()->except('view')->resource(array('submodule' => 'admin', 'bundle' => 'users'));
-Route::get('admin/singin', array('as' => 'users_admin_default_signin', 'uses' => 'users::admin.default@signin'));
-Route::post('admin/singin', array('as' => 'users_admin_default_signin', 'uses' => 'users::admin.default@signin'));
+RestfulRouter::get('admin/singin', array('as' => 'users_admin_default_signin', 'uses' => 'users::admin.default@signin'));
+RestfulRouter::post('admin/singin', array('as' => 'users_admin_default_signin', 'uses' => 'users::admin.default@signin'));
 
 /* Groups */
 RestfulRouter::make()

6574250795fdc5a90ee39692f0e2084058cde34f
Armen Markossyan <a.a.markossyan@gmail.com>
2014-06-05 18:10:18 +0600

    Improves Dropdown Filter

--- Mobileka/L3/Engine/Grid/Filters/Dropdown.php
+++ Mobileka/L3/Engine/Grid/Filters/Dropdown.php
@@ -1,7 +1,7 @@
 <?php namespace Mobileka\L3\Engine\Grid\Filters;
 
-use Mobileka\L3\Engine\Laravel\Lang,
-	Mobileka\L3\Engine\Laravel\Helpers\Arr;
+use Mobileka\L3\Engine\Laravel\Lang;
+use	Mobileka\L3\Engine\Laravel\Helpers\Arr;
 
 class Dropdown extends BaseComponent {
 
@@ -14,6 +14,14 @@ class Dropdown extends BaseComponent {
 
 	public function options($options, $defaultValue = true)
 	{
+		if ($this->translate)
+		{
+			foreach ($options as $key => $option)
+			{
+				$options[$key] = Lang::findLine($this->languageFile, $option);
+			}
+		}
+
 		if ($defaultValue)
 		{
 			$options = array(null => Lang::findLine($this->languageFile, 'not_selected')) + $options;

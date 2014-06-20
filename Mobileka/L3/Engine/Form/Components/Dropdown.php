6acc49b52e62b026de3fe860ba2c437e23337189
Armen Markossyan <a.a.markossyan@gmail.com>
2014-06-20 15:12:01 +0600

    Improves Dropdown component

--- Mobileka/L3/Engine/Form/Components/Dropdown.php
+++ Mobileka/L3/Engine/Form/Components/Dropdown.php
@@ -1,60 +1,53 @@
 <?php namespace Mobileka\L3\Engine\Form\Components;
 
 use Mobileka\L3\Engine\Laravel\Lang;
+use Mobileka\L3\Engine\Laravel\Helpers\Arr;
 
-class Dropdown extends BaseComponent {
+class Dropdown extends BaseComponent
+{
+    protected $template = 'engine::form.dropdown';
+    protected $htmlElement = 'select';
+    protected $options = array();
 
-	protected $template = 'engine::form.dropdown';
-	protected $htmlElement = 'select';
-	protected $options = array();
+    public function options($options, $defaultValue = true)
+    {
+        if ($this->translate) {
+            foreach ($options as $key => $option) {
+                $options[$key] = Lang::findLine($this->languageFile, $option);
+            }
+        }
 
-	public function options($options, $defaultValue = true)
-	{
-		if ($this->translate)
-		{
-			foreach ($options as $key => $option)
-			{
-				$options[$key] = Lang::findLine($this->languageFile, $option);
-			}
-		}
+        if ($defaultValue) {
+            $options = array(null => Lang::findLine($this->languageFile, 'not_selected')) + $options;
+        }
 
-		if ($defaultValue)
-		{
-			$options = array(null => Lang::findLine($this->languageFile, 'not_selected')) + $options;
-		}
+        $this->options = $options;
 
-		$this->options = $options;
+        return $this;
+    }
 
-		return $this;
-	}
+    public function selectboxName()
+    {
+        return Arr::getItem($this->attributes, 'multiple') ? $this->name . '[]' : $this->name;
+    }
 
-	public function selectboxName()
-	{
-		return isset($this->attributes['multiple']) ? $this->name . '[]' : $this->name;
-	}
+    /**
+     * Returns a value of a component.
+     */
+    public function value($lang = '')
+    {
+        $value = $this->row;
 
-	/**
-	 * Returns a value of a component.
-	 */
-	public function value($lang = '')
-	{
-		$value = $this->row;
+        if (Arr::getItem($this->attributes, 'multiple') === 'multiple') {
+            $value = $value->{$this->name}()->lists('id');
+        } else {
+            $tokens = explode('.', $this->name);
 
-		if (isset($this->attributes['multiple']) and $this->attributes['multiple'] === 'multiple')
-		{
-			$value = $value->{$this->name}()->lists('id');
-		}
-		else
-		{
-			$tokens = explode('.', $this->name);
+            foreach ($tokens as $token) {
+                $value = $value->{$token};
+            }
+        }
 
-			foreach ($tokens as $token)
-			{
-				$value = $value->{$token};
-			}
-		}
-
-		return $value;
-	}
-
+        return $value;
+    }
 }

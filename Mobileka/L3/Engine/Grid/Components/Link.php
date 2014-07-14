ec3148d2ec808f40eac4d0483bf26ef11cf56ee7
Armen Markossyan <a.a.markossyan@gmail.com>
2014-07-14 12:44:40 +0600

    Improves Link component

--- Mobileka/L3/Engine/Grid/Components/Link.php
+++ Mobileka/L3/Engine/Grid/Components/Link.php
@@ -1,35 +1,35 @@
 <?php namespace Mobileka\L3\Engine\Grid\Components;
 
-class Link extends BaseComponent {
+class Link extends BaseComponent
+{
+    protected $template = 'engine::grid.link';
+    protected $params   = array();
+    protected $label = '';
+    protected $beforeLabel = '';
+    protected $afterLabel = '';
+    protected $route    = '';
+    protected $link;
+    protected $query_params = array();
 
-	protected $template = 'engine::grid.link';
-	protected $params   = array();
-	protected $label;
-	protected $route    = '';
-	protected $link;
-	protected $query_params = array();
+    public function getLabel()
+    {
+        if (!is_null($this->label)) {
+            return $this->normalizeAttributeValue($this->label);
+        }
 
-	public function getLabel()
-	{
-		if (!is_null($this->label))
-		{
-			return $this->normalizeAttributeValue($this->label);
-		}
-		return $this->row->{$this->name};
-	}
+        return $this->row->{$this->name};
+    }
 
-	public function getLink()
-	{
-		if (!is_null($this->link))
-		{
-			return $this->normalizeAttributeValue($this->link);
-		}
-		$url = \URL::to_route($this->route, $this->params);
-		if ($this->query_params)
-		{
-			$url .= '?' . http_build_query($this->normalizeAttributeValue($this->query_params));
-		}
+    public function getLink()
+    {
+        if (!is_null($this->link)) {
+            return $this->normalizeAttributeValue($this->link);
+        }
+        $url = \URL::to_route($this->route, $this->params);
+        if ($this->query_params) {
+            $url .= '?' . http_build_query($this->normalizeAttributeValue($this->query_params));
+        }
 
-		return $url;
-	}
+        return $url;
+    }
 }
--- Mobileka/L3/Engine/views/grid/link.blade.php
+++ Mobileka/L3/Engine/views/grid/link.blade.php
@@ -1,7 +1,7 @@
 <?php try { ?>
 
 <div class="text-center">
-	<a href="{{{ $component->link }}}" {{ HTML::attributes($component->attributes) }}>{{{ $component->label }}}</a>
+	<a href="{{{ $component->link }}}" {{ HTML::attributes($component->attributes) }}>{{ $component->beforeLabel }}{{ $component->label }}{{ $component->afterLabel }}</a>
 </div>
 
 <?php } catch (Exception $e) { exit($e->getMessage()); } ?>
